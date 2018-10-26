70ec90a3f072f4511c546aa1f5937610e9d214a9
도우진 <dwj1996@naver.com>
2018-10-26 18:59:27 +0900

    Complete the solution

--- src/solveAMazeWithStack/MazeCoord.java
+++ src/solveAMazeWithStack/MazeCoord.java
@@ -1,13 +1,77 @@
 package solveAMazeWithStack;
 
 public class MazeCoord {
-	public int i = 0;
-	public int j = 0;
-	public int dir = 0;
+	public int i;
+	public int j;
+	public int dir;
+	// Direction
+	public static final int NORTH = 0;
+	public static final int EAST = 1;
+	public static final int SOUTH = 2;
+	public static final int WEAST = 3;
 	
 	MazeCoord(int i, int j, int dir) {
 		this.i = i;
 		this.j = j;
 		this.dir = dir;
+	}
+	
+	// For a call by value
+	MazeCoord(MazeCoord mazeCoord) {
+		this.i = mazeCoord.i;
+		this.j = mazeCoord.j;
+		this.dir = mazeCoord.dir;
+	}
+
+	public boolean move() {
+		switch(dir) {
+		case NORTH :
+			i--;
+			break;
+		case EAST :
+			j++;
+			break;
+		case SOUTH :
+			i++;
+			break;
+		case WEAST :
+			j--;
+			break;
+		default :
+			return false;
+		}
+		if(i >= 0 && j >= 0) return true;
+		else return false;
+	}
+	public boolean move(int dir) {
+		this.dir = dir;
+		switch(dir) {
+		case NORTH :
+			i--;
+			break;
+		case EAST :
+			j++;
+			break;
+		case SOUTH :
+			i++;
+			break;
+		case WEAST :
+			j--;
+			break;
+		default :
+			return false;
+		}
+		if(i >= 0 && j >= 0) return true;
+		else return false;
+	}
+
+	public void print() {
+		System.out.println("i : " + i + ", j :" + j + ", Direction : " + dir);
+	}
+	public void printToArrayPostion() {
+		System.out.println("(" + i + ", " + j + ")");
+	}
+	public void printToRealPosition() {
+		System.out.println("(" + (i-1) + ", " + (j-1) + ")");
 	}
 }
--- src/solveAMazeWithStack/SolveAMazeWithStack.java
+++ src/solveAMazeWithStack/SolveAMazeWithStack.java
@@ -1,39 +1,85 @@
 package solveAMazeWithStack;
 
 public class SolveAMazeWithStack {
-	private static final int M = 6;
+	// M * N Maze
+	private static final int M = 7;
 	private static final int N = 6;
 	
-	// Direction
-	private static final int NORTH = 0;
-	private static final int EAST = 1;
-	private static final int SOUTH = 2;
-	private static final int WEAST = 3;
-	
 	public static void main(String[] args) {
-		mazePath();
+		DataStack pathStack = mazePath();
+		if(pathStack == null) return;
+		else {
+			MazeCoord[] mazePath = new MazeCoord[pathStack.GetSize()];
+			while(!pathStack.IsEmpty()) {
+				mazePath[pathStack.GetSize()-1] = pathStack.Pop();
+			}
+			for(MazeCoord temp : mazePath)
+				temp.printToArrayPostion();
+		}
 	}
+
 	
-	private static void mazePath() {
-		// Initialize
-		int[][] maze = new int[M+2][N+2];
+	private static DataStack mazePath() {
+		/**
+		 * Initialize
+		 * Please initialize the your maze.
+		 * Maze's first and last rows and columns are must 1.
+		 * You must initialize index of 1~last-1. The array's size is [M+2][N+2].
+		 * 0 : pass possible, road 1 : impossible, wall.
+		 */
+		int[][] maze = 
+				{ {1, 1, 1, 1, 1, 1, 1, 1}, 
+				  {1, 0, 1, 1, 0, 1, 1, 1},
+				  {1, 0, 0, 1, 0, 1, 1, 1}, 
+				  {1, 1, 0, 0, 0, 1, 1, 1}, 
+				  {1, 1, 1, 0, 0, 0, 1, 1}, 
+				  {1, 0, 0, 0, 1, 1, 1, 1}, 
+				  {1, 1, 0, 1, 0, 0, 0, 1},
+			      {1, 0, 0, 0, 0, 1, 0, 1}, 
+				  {1, 1, 1, 1, 1, 1, 1, 1} };
 		int[][] mark = new int[M+2][N+2];
+		for(int i = 0; i < mark.length; i++)
+			for(int j = 0; j < mark[0].length; j++)
+				mark[i][j] = 0;
 		
+		// print initialized arrays.
+		System.out.println("==== MAZE ====");
 		for(int i = 0; i < maze.length; i++) {
 			for(int j = 0; j < maze[0].length; j++) {
-				maze[i][j] = mark[i][j] = 0;
-				if((i == 0) || (i == maze.length-1) || 
-						(j == 0) || (j == maze[0].length-1)) {
-					maze[i][j] = 1;
+				System.out.print(maze[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		// Solve the maze.
+		DataStack mazeStack = new DataStack(M * N);
+		mazeStack.Push(new MazeCoord(1, 1, MazeCoord.EAST));		// Entrance. The direction of movement is initialized to the east.
+		mark[1][1] = 1;
+		
+		MazeCoord current, next;
+		while(!mazeStack.IsEmpty()) {
+			current = mazeStack.Pop();
+			while(current.dir <= MazeCoord.WEAST) {
+				next = new MazeCoord(current);
+				next.move();
+				if(next.i == M && next.j == N) {
+					mazeStack.Push(current);
+					mazeStack.Push(next);
+					System.out.println("The path is as follows.");
+					return mazeStack;
 				}
+				if(maze[next.i][next.j] == 0 &&			// Can Move
+						mark[next.i][next.j] == 0) {	// Didn't try
+					mark[next.i][next.j] = 1;
+					mazeStack.Push(current);
+					current = new MazeCoord(next);
+					current.dir = MazeCoord.NORTH;
+				} 
+				else current.dir++;
 			}
 		}
-		
-		DataStack mazeStack = new DataStack(M * N);
-		mazeStack.Push(new MazeCoord(1, 1, EAST));		// Entrance. The direction of movement is initialized to the east.
-		
-		while(!mazeStack.IsEmpty()) {
-			MazeCoord current = mazeStack.Pop();
-		}
+		System.out.println("There is no path");
+		return null;
 	}
 }
